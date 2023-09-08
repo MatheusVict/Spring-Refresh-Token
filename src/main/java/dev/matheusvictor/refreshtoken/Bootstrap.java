4d1d64eac7000bc3283a305af049b327bf937b8e
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-08 16:22:01 -0300

    feat: inial commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/dev/matheusvictor/refreshtoken/Bootstrap.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.refreshtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstrap.class, args);
+	}
+
+}
